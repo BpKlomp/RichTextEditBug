5f30e2ba470fd92335d29d8f6504f263e185a5a2
BpKlomp <101294255+BpKlomp@users.noreply.github.com>
2024-05-22 11:25:07 +0200

    Add files via upload

---
+++ RichTextEditBug/RichTextEditBug/Models/TextObject.cs
@@ -0,0 +1,19 @@
+﻿namespace RichTextEditBug.Models
+{
+    public class TextObject
+    {
+
+        public string ComponentType { get; set; } = "Component1";
+        public string Text { get; set; } = string.Empty;
+        public int Amount { get; set; } = 0;
+        public TextObject(string componentType, string text, int amount = 0)
+        {
+            ComponentType = componentType;
+            Text = text;
+            Amount = amount;
+        }
+        public TextObject() { }
+
+
+    }
+}
---
+++ RichTextEditBug/RichTextEditBug/Program.cs
@@ -0,0 +1,36 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Blazorise.RichTextEdit;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons()
+    .AddBlazoriseRichTextEdit();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
